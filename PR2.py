2a73c581170021b99a0bca1d16092e2f5dff0328
Hotaru654 <114014175+Hotaru654@users.noreply.github.com>
2022-09-20 20:20:14 +0300

    Add files via upload

---
+++ PR2.py
@@ -0,0 +1,7 @@
+import math
+
+x = 14.26
+y = -1.22
+z = 3.5*10**-2
+s = (2 * math.cos(x - 2/3)) / (1/2 + pow(math.sin(y), 2)) * (1 + (pow(z, 2) / 3 - pow(z, 2) // 5))
+print("s = {0:.3f}".format(s))
