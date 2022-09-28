b9f0b2a98a4d41fb4645319c16ab007454969700
Hotaru654 <114014175+Hotaru654@users.noreply.github.com>
2022-09-28 18:41:57 +0300

    Add files via upload

---
+++ PR4.py
@@ -0,0 +1,26 @@
+"""
+a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+	print(i)
+"""
+
+"""
+a = int(input())
+b = int(input())
+if b < a:
+	for i in range(a, b - 1, -1):
+		print(i)	
+elif a < b:
+	for i in range(a, b + 1):
+		print(i)
+"""
+
+"""
+a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+	if i % 2 != 0:
+		print(i)
+"""
+
